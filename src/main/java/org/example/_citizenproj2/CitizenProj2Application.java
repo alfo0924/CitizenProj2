c41dc55831b8feb91ec9c4f432f1ac78e99c1b9d
alfo0924 <halo0924@yahoo.com.tw>
2024-10-30 11:53:33 +0800

    v1.01 cnp

---
+++ src/main/java/org/example/_citizenproj2/CitizenProj2Application.java
@@ -0,0 +1,13 @@
+package org.example._citizenproj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitizenProj2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CitizenProj2Application.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/_citizenproj2/CitizenProj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example._citizenproj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitizenProj2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
